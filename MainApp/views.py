80e83f6b83084ac9c258e8dbb6e0a36c3a538968
Blake Ehlinger <blake_ehlinger1@baylor.edu>
2022-04-25 15:56:50 -0500

    More class work_

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Topic 
 # Create your views here.
+
+def index(request):
+    return render(request, 'MainApp/index.html')
+
+def topics(request):
+    topics = Topic.objects.all()
+
+    context = {'topics':topics}
+
+    return render(request,'MainApp/topics.html', context)
+    # key value on line 11: what we use on key is what we use in html and the value is what we use in the view file
